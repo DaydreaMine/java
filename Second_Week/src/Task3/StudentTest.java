57acc538e13d6599ad3ce02443104a3b17af39ae
DaydreaMine <60591189+DaydreaMine@users.noreply.github.com>
2020-03-09 23:26:26 +0800

    Monday

--- Second_Week/src/Task3/StudentTest.java
+++ Second_Week/src/Task3/StudentTest.java
@@ -2,7 +2,6 @@ import java.util.*;
 
 public class StudentTest {
 
-    @SuppressWarnings("resource")
     public static void main(String[] args) {
         Student student = new Student();
         String[] arr1 = new String[]{"李雷", "韩梅梅", "小明", "老王", "大黄"};
@@ -11,31 +10,30 @@ public class StudentTest {
         int[][] arr = new int[student.getStudent_Num()][student.getStudent_courseNum()];
         //初始化总分数组
         int[] sum = new int[student.getStudent_Num()];
-        int sum1 = 0;//总分初始化
-        int num1 = 0;//总分最高的下标
-        int num2 = 0;//理综最高的下标
-
+        
         // 输入各科成绩，并求每个人的总成绩
         for (int i = 0; i < student.getStudent_Num(); i++) {
+            int sum1 = 0;//总分初始化
             for (int j = 0; j < student.getStudent_courseNum(); j++) {
                 //键盘输入
-                Scanner sc = new Scanner(System.in);
-                int a = sc.nextInt();
-                student.setCourse_Result(a);
-                arr[i][j] = student.getCourse_Result();
                 student.setName(arr1[i]);
                 student.setCourse(arr2[j]);
                 System.out.println("第" + (i + 1) + "位:" + student.getStudent_Name() + "的" + student.getStudent_course() + "成绩为:");
+                Scanner sc = new Scanner(System.in);
+                int a = sc.nextInt();
+                student.setCourse_Result(a);
                 arr[i][j] = student.getCourse_Result();
                 sum1 += arr[i][j];
             }
             sum[i] = sum1;
         }
 
+        int num1 = 0;//总分最高的下标
+        int num2 = 0;//理综最高的下标
         //三目算法求总分最高和理综最高的人
         for (int k = 0; k < student.getStudent_Num() - 1; k++) {
-            num1 = (sum[k] > sum[k + 1]) ? k : k + 1;
-            num2 = (arr[k][4] > arr[k + 1][4]) ? k : k + 1;
+            num1 = (sum[num1] > sum[k + 1]) ? num1 : k + 1;
+            num2 = (arr[k][3] > arr[k + 1][3]) ? num1 : k + 1;
         }
         student.setName(arr1[num1]);
         System.out.println("平均分最高的是：" + student.getStudent_Name());
