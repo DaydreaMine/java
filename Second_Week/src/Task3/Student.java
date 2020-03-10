0a957eede404f27a1d0118e5672d5fee5fa9b2e5
DaydreaMine <60591189+DaydreaMine@users.noreply.github.com>
2020-03-10 10:00:59 +0800

    修改

--- Second_Week/src/Task3/Student.java
+++ Second_Week/src/Task3/Student.java
@@ -1,49 +1,44 @@
 public class Student {
 
-    private String Student_Name;        //姓名
-    private String Student_course;      //科目
-    private int Student_courseNum = 4;  //科目数量
-    private int Student_Num = 5;        //学生数量
-    private int course_Result;          //科目成绩
+	private String Student_Name; // 姓名
+	private String Student_course; // 科目
+	private int Student_courseNum = 4; // 科目数量
+	private int course_Result; // 科目成绩
+	public void setName(String Student_Name) {
+		this.Student_Name = Student_Name;
+	}
 
-    public void setName(String Student_Name) {
-        this.Student_Name = Student_Name;
-    }
+	public void setCourse(String Student_course) {
+		this.Student_course = Student_course;
+	}
 
-    public void setCourse(String Student_course) {
-        this.Student_course = Student_course;
-    }
+	public void setCourseNum(int Studeng_courseNum) {
+		this.Student_courseNum = Studeng_courseNum;
+	}
 
-    public void setResult(int Student_Num) {
-        this.Student_Num = Student_Num;
-    }
+	public void setCourse_Result(int course_Result) {
+		if (course_Result > 0 && course_Result < 101) {
+			this.course_Result = course_Result;
+		} else {
+			course_Result = 60;
+			System.out.println("成绩输入错误！");
+		}
+	}
 
-    public void setCourseNum(int Studeng_courseNum) {
-        this.Student_courseNum = Studeng_courseNum;
-    }
+	public String getStudent_Name() {
+		return this.Student_Name;
+	}
 
-    public void setCourse_Result(int course_Result) {
-        this.course_Result = course_Result;
-    }
+	public String getStudent_course() {
+		return this.Student_course;
+	}
 
-    public String getStudent_Name() {
-        return this.Student_Name;
-    }
+	public int getStudent_courseNum() {
+		return this.Student_courseNum;
+	}
 
-    public int getStudent_Num() {
-        return this.Student_Num;
-    }
-
-    public String getStudent_course() {
-        return this.Student_course;
-    }
-
-    public int getStudent_courseNum() {
-        return this.Student_courseNum;
-    }
-
-    public int getCourse_Result() {
-        return this.course_Result;
-    }
+	public int getCourse_Result() {
+		return this.course_Result;
+	}
 
 }
--- Second_Week/src/Task3/StudentTest.java
+++ Second_Week/src/Task3/StudentTest.java
@@ -2,48 +2,48 @@ import java.util.*;
 
 public class StudentTest {
 
-    public static void main(String[] args) {
-        Student student = new Student();
-        String[] arr1 = new String[]{"李雷", "韩梅梅", "小明", "老王", "大黄"};
-        String[] arr2 = new String[]{"语文", "数学", "英语", "理科综合"};
-        //将每门分数放到二维数组中
-        int[][] arr = new int[student.getStudent_Num()][student.getStudent_courseNum()];
-        //初始化总分数组
-        int[] sum = new int[student.getStudent_Num()];
-        
-        // 输入各科成绩，并求每个人的总成绩
-        for (int i = 0; i < student.getStudent_Num(); i++) {
-            int sum1 = 0;//总分初始化
-            for (int j = 0; j < student.getStudent_courseNum(); j++) {
-                //键盘输入
-                student.setName(arr1[i]);
-                student.setCourse(arr2[j]);
-                System.out.println("第" + (i + 1) + "位:" + student.getStudent_Name() + "的" + student.getStudent_course() + "成绩为:");
-                Scanner sc = new Scanner(System.in);
-                int a = sc.nextInt();
-                student.setCourse_Result(a);
-                arr[i][j] = student.getCourse_Result();
-                sum1 += arr[i][j];
-            }
-            sum[i] = sum1;
-        }
+	public static void main(String[] args) {
+		Student student = new Student();
+		String[] arr1 = new String[] { "李雷", "韩梅梅", "小明", "老王", "大黄" };
+		String[] arr2 = new String[] { "语文", "数学", "英语", "理科综合" };
+		// 将每门分数放到二维数组中
+		int[][] arr = new int[5][student.getStudent_courseNum()];
+		// 初始化总分数组
+		int[] sum = new int[5];
+		// 输入各科成绩，并求每个人的总成绩
+		for (int i = 0; i < 5; i++) {
+			int sum1 = 0;// 总分初始化
+			for (int j = 0; j < student.getStudent_courseNum(); j++) {
+				// 键盘输入
+				student.setName(arr1[i]);
+				student.setCourse(arr2[j]);
+				System.out.print(
+						"第" + (i + 1) + "位:" + student.getStudent_Name() + "的" + student.getStudent_course() + "成绩为: ");
+				Scanner sc = new Scanner(System.in);
+				int a = sc.nextInt();
+				student.setCourse_Result(a);
+				arr[i][j] = student.getCourse_Result();
+				sum1 += arr[i][j];
+			}
+			sum[i] = sum1;
+		}
 
-        int num1 = 0;//总分最高的下标
-        int num2 = 0;//理综最高的下标
-        //三目算法求总分最高和理综最高的人
-        for (int k = 0; k < student.getStudent_Num() - 1; k++) {
-            num1 = (sum[num1] > sum[k + 1]) ? num1 : k + 1;
-            num2 = (arr[k][3] > arr[k + 1][3]) ? num1 : k + 1;
-        }
-        student.setName(arr1[num1]);
-        System.out.println("平均分最高的是：" + student.getStudent_Name());
-        System.out.println("各科成绩是：");
-        for (int l = 0; l < student.getStudent_courseNum(); l++) {
-            System.out.println(arr[num1][l]);
-        }
+		int num1 = 0;// 总分最高的下标
+		int num2 = 0;// 理综最高的下标
+		// 三目算法求总分最高和理综最高的人
+		for (int k = 0; k < 4; k++) {
+			num1 = (sum[num1] > sum[k + 1]) ? num1 : k + 1;
+			num2 = (arr[k][3] > arr[k + 1][3]) ? num1 : k + 1;
+		}
+		student.setName(arr1[num1]);
+		System.out.println("平均分最高的是：" + student.getStudent_Name());
+		System.out.println("各科成绩是：");
+		for (int l = 0; l < student.getStudent_courseNum(); l++) {
+			System.out.println(arr[num1][l]);
+		}
 
-        student.setName(arr1[num2]);
-        System.out.println("理综最高的是：" + student.getStudent_Name());
+		student.setName(arr1[num2]);
+		System.out.println("理综最高的是：" + student.getStudent_Name());
 
-    }
+	}
 }
