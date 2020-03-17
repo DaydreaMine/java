37f3fec6dc7c53ba5122a585fdb299074e3ef1d6
DaydreaMine <60591189+DaydreaMine@users.noreply.github.com>
2020-03-17 20:50:57 +0800

    test

---
+++ Third_Week/Tuesday/Throw$Throws.java
@@ -0,0 +1,13 @@
+/**
+ * @author DaydreaminE
+ * @version 创建时间：2020年3月17日 下午8:47:39 类说明
+ */
+public class Throw$Throws {
+	/*
+	 * throw用于抛出异常,就是抛出一个空指针异常。 除非用try
+	 * catch包围起来，会在执行完catch和finally块中的代码后，继续执行之后的代码。这也是try catch的一个作用，即防止程序崩溃
+	 * 
+	 * throws用于声明一个方法可能抛出某种异常，提醒调用者处理。
+	 */
+
+}
